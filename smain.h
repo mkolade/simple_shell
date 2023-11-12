cfa59c54b7dd274612be5acf026047aa1b041d08
mkolade <mohammedamoshood@gmail.com>
2023-11-12 07:35:05 -0800

    added struct data

--- smain.h
+++ smain.h
@@ -13,4 +13,32 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+
+#define TOKEN_DELIM " \t\r\n\a"
+#define TOKEN_SIZE 128
+#define BUFSIZE 1024
+
+/* This Points to an array of pointers to strings called the "environment" */
+extern char **environ;
+
+/**
+ * struct data - A struct that contains all relevant data on runtime
+ * @av: argument vector
+ * @input: command line written by user
+ * @args: arguments from command line
+ * @status: holds the last status of the shell
+ * @counter: line counter
+ * @_environ: environment variable
+ * @pid: process ID of the shell
+ */
+typedef struct data
+{
+	char **av;
+	char *input;
+	char **args;
+	int status;
+	int counter;
+	char **_environ;
+	char *pid;
+} shell_data;
 #endif /* smain.h */

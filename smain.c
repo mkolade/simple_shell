97fe602e9649dc6958d179bea06c67d7bc084881
mkolade <mohammedamoshood@gmail.com>
2023-11-12 07:59:11 -0800

    added main function to smain.c

--- smain.c
+++ smain.c
@@ -1,1 +1,22 @@
-jkfvnjdk
+#include "smain.h"
+
+/**
+ * main - Main entry point
+ * @ac: number of argument count
+ * @av: number of argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	shell_data shdata;
+	(void) ac;
+
+	signal(SIGINT, getSignal);
+	setData(&shdata, av);
+	shellPrompt(&shdata);
+	freeData(&shdata);
+	if (shdata.status < 0)
+		return (255);
+	return (shdata.status);
+}

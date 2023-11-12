ddb871c53a00dc98e7c193f1c451b583d33bb722
mkolade <mohammedamoshood@gmail.com>
2023-11-12 07:26:53 -0800

    created header file and installed all neccessary libraries

---
+++ smain.h
@@ -0,0 +1,16 @@
+#ifndef _SMAIN_H_
+#define _SMAIN_H_
+
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <signal.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#endif /* smain.h */

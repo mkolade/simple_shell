83466e2695ba02f4406491eeeea94f84c3c6f1f0
mkolade <mohammedamoshood@gmail.com>
2023-11-15 19:17:21 -0800

    assignLine getLine _get_line

--- read_input.c
+++ read_input.c
@@ -31,3 +31,67 @@ void assignLine(char **lineptr, size_t *n, char *buffer, size_t j)
 		free(buffer);
 	}
 }
+
+/**
+ * getLine - Read input from stream
+ * @lineptr: Buffer that stores the input
+ * @n: Size of lineptr
+ * @stream: Stream to read from
+ * Return: The number of bytes read
+ */
+ssize_t getLine(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == NULL)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
+	}
+	buffer[input] = '\0';
+	assignLine(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
+}
+/**
+ * _get_line - reads the input string.
+ *
+ * @i_eof: return value of getline function
+ * Return: input string
+ */
+char *_get_line(int *i_eof)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*i_eof = getline(&input, &bufsize, stdin);
+	return (input);
+}

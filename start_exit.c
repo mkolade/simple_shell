e31b5b5252ed94ced8ae244dbba2cfab8ce0079c
mkolade <mohammedamoshood@gmail.com>
2023-11-15 19:12:02 -0800

    made final edits to start_exit.c and added exit_shell

--- start_exit.c
+++ start_exit.c
@@ -20,3 +20,33 @@ int exec_line(shell_data *shdata)
 
 	return (cmd_execve(shdata));
 }
+
+/**
+ * exit_shell - exits the shell
+ *
+ * @shdata: data relevant (status and args)
+ * Return: 0 on success.
+ */
+int exit_shell(shell_data *shdata)
+{
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
+
+	if (shdata->args[1] != NULL)
+	{
+		ustatus = _atoi(shdata->args[1]);
+		is_digit = _isdigit(shdata->args[1]);
+		str_len = _strlen(shdata->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			error_code(shdata, 2);
+			shdata->status = 2;
+			return (1);
+		}
+		shdata->status = (ustatus % 256);
+	}
+	return (0);
+}

c8bdeb1ffd58d2f35e3b591e5c3eb4201ba86641
mkolade <mohammedamoshood@gmail.com>
2023-11-17 18:50:59 -0800

    made small edits to _strchr and _strspn

--- more_string.c
+++ more_string.c
@@ -46,3 +46,77 @@ char *_strcpy(char *dest, const char *src)
 
 	return (dest);
 }
+
+/**
+ * _strcmp - Compare two strings.
+ *
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: Negative value if s1 is less than s2,
+ * positive value if s1 is greater than s2, 0 if s1 is equal to s2.
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
+		;
+
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+
+/**
+ * _strchr - Locate a character in a string.
+ *
+ * @s: String to search in.
+ * @c: Character to locate.
+ *
+ * Return: Pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found.
+ */
+char *_strchr(const char *s, int c)
+{
+	unsigned int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return ((char *)(s + i));
+	if (s[i] == c)
+		return ((char *)(s + i));
+	return (NULL);
+}
+
+/**
+ * _strspn - Get the length of a prefix substring.
+ *
+ * @s: Initial segment.
+ * @accept: Accepted bytes.
+ *
+ * Return: Number of accepted bytes.
+ */
+size_t _strspn(const char *s, const char *accept)
+{
+	size_t i, j;
+	int bool;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
